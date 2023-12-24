c407a3aa9f920d2b482f6f5b75b94e0f73128f81
edgomes20 <edgomes20@hotmail.com>
2023-12-23 21:51:41 -0300

    Aula 03

--- main.js
+++ main.js
@@ -7,5 +7,9 @@ function tocaSomClap(){
     document.querySelector('#som_tecla_clap').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+// document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
 
