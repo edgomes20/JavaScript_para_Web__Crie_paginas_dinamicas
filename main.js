b4e11bb6c3758354ea096e52a7b11a625970e495
edgomes20 <edgomes20@hotmail.com>
2023-12-23 22:28:24 -0300

    Aula 04

--- main.js
+++ main.js
@@ -1,15 +1,53 @@
 
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
+/*
 function tocaSomClap(){
     document.querySelector('#som_tecla_clap').play();
 }
+*/
 
 // document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+
+/*
+let contador = 0;
+
+// Enquanto
+while(contador < listaDeTeclas.length){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    // Template String
+    const idAudio = `#som_${instrumento}`;
+    //console.log(idAudio);
+
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    }
+    contador++;
+
+    //console.log(contador);
+}
+*/
+
+// For
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    // Template String
+    const idAudio = `#som_${instrumento}`;
+    //console.log(idAudio);
+
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    }
+
+    //console.log(contador);
+}
 
